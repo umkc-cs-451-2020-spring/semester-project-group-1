ea799ad9bb81a14bd50d2ef6619bf7cdf83bb131
Tars <taa522@Mail.umkc.edu>
2020-02-25 19:45:16 -0600

    Added Spring Boot Starter project

---
+++ TTTParent/transaction-trigger-tracker/src/main/java/com/commercecapstone/transactiontriggertracker/TransactionTriggerTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.commercecapstone.transactiontriggertracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionTriggerTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionTriggerTrackerApplication.class, args);
+	}
+
+}
---
+++ TTTParent/transaction-trigger-tracker/src/test/java/com/commercecapstone/transactiontriggertracker/TransactionTriggerTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commercecapstone.transactiontriggertracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionTriggerTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
