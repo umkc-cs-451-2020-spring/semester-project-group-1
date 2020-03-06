49a3089f700826d9c20538e6212993155a30bb6c
Ekrem <ekremkalabak@gmail.com>
2020-03-05 18:28:16 -0600

    Created TransactionDomain

--- TTTParent/transaction-trigger-tracker/src/main/java/com/commercecapstone/transactiontriggertracker/domain/StateTriggerDomain.java
+++ TTTParent/transaction-trigger-tracker/src/main/java/com/commercecapstone/transactiontriggertracker/domain/StateTriggerDomain.java
@@ -1,25 +1,5 @@
 package com.commercecapstone.transactiontriggertracker.domain;
 
-import java.sql.Date;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeFormatterBuilder;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.sun.corba.se.spi.orbutil.fsm.Guard.Result;
-
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-
-
-//@Data @AllArgsConstructor @Builder
 public class StateTriggerDomain {
-	private int User_ID;
-	private int Trigger_ID;
-	private int Account_ID;
-	private int Transactions_ID;
-	private int Notification_ID;
 	
 }
