a7a07a26e67884fc2b7ce48fdd290ddbe80d7ce0
Ekrem <ekremkalabak@gmail.com>
2020-04-07 22:35:29 -0500

    Updated Transaction DAO

--- TTTParent/transaction-trigger-tracker/src/main/java/com/commercecapstone/transactiontriggertracker/dao/TransactionDao.java
+++ TTTParent/transaction-trigger-tracker/src/main/java/com/commercecapstone/transactiontriggertracker/dao/TransactionDao.java
@@ -51,17 +51,46 @@ public class TransactionDao extends BaseDao{
      * @param int accountId
      * @return TransactionDomain
      */
-    public TransactionDomain getTransaction(int transactionID) {
-    	TransactionDomain transaction = null;
-        List<TransactionDomain> transactionWrapper = null;
+    public List<TransactionDomain> getAccountTransactions(int inputAccountID) {
+    	List<TransactionDomain> accountTransactions = null;
         
-        String typeQuery = "select * from Transaction where Transaction_ID = :inputTransaction_ID";
+        String typeQuery = "select * from Transaction where Account_ID = :inAccount_ID";
         HashMap<String,Object> params = new HashMap<String,Object>();
-        params.put("inputTransaction_ID", transactionID);
+        params.put("inAccount_ID", inputAccountID);
+        
+        try {
+        	accountTransactions = get().query(typeQuery, params, transactionMapper);
+            log.info("Transactions for AccountID: {} successfully retrieved", inputAccountID);
+        }
+        catch(NullPointerException e) {
+            log.error(e.getMessage());
+            e.printStackTrace();
+        }
+        catch(DataAccessException e) {
+            log.error(e.getMessage());
+            e.printStackTrace();
+        }
+        catch(Exception e) {
+            log.error(e.getMessage());
+            e.printStackTrace();
+        }
+        
+        return accountTransactions;
+        
+    }
+
+    public TransactionDomain getTransaction(int inputAccountID, int inputTransactionID) {
+    	List<TransactionDomain> transactionWrapper = null;
+        
+        String typeQuery = "select * from Transaction where Transaction_ID = :inTransaction_ID and Account_ID = :inAccount_ID";
+        HashMap<String,Object> params = new HashMap<String,Object>();
+        params.put("inTransaction_ID", inputTransactionID);
+        params.put("inAccount_ID", inputAccountID);
+        
         
         try {
         	transactionWrapper = get().query(typeQuery, params, transactionMapper);
-            log.info("Application of TransactionID: {} successfully retrieved", transactionID);
+            log.info("TransactionID: {} of Account ID: {} successfully retrieved", inputTransactionID, inputAccountID);
         }
         catch(NullPointerException e) {
             log.error(e.getMessage());
@@ -80,14 +109,24 @@ public class TransactionDao extends BaseDao{
         
     }
     
-    public ResponseEntity<Object> addTransaction(Map<String, Object> inputParams){
+    public ResponseEntity<Object> addTransaction(TransactionDomain inputTransaction){
 
-        String typeQuery = "INSERT INTO Transaction(Transaction_ID, Transaction_type, Transaction_time, State, Category, Transaction_description, Ammount, Account_ID) " +
-                "VALUES (:inputTransaction_ID, :inputTransaction_type, :inputTransaction_time, :inputState, :inputCategory, :inputTransaction_description, :inputAmmount, :inputAccount_ID) " + 
-                "ON DUPLICATE KEY UPDATE Transaction_ID = :inputTransaction_ID, Transaction_type = :inputTransaction_type, Transaction_time = :inputTransaction_time, State = :inputState, Category = :inputCategory, Transaction_description = :inputTransaction_description, Ammount = :inputAmmount, Account_ID = :inputAccount_ID; " ;
+        String typeQuery = "INSERT INTO Transaction(Transaction_type, Transaction_time, State, Category, Transaction_description, Ammount, Account_ID) " +
+                "VALUES (:inTransaction_type, :inTransaction_time, :inState, :inCategory, :inTransaction_description, :inAmmount, :inAccount_ID) " + 
+                "ON DUPLICATE KEY UPDATE Transaction_type = :inTransaction_type, Transaction_time = :inTransaction_time, State = :inState, Category = :inCategory, Transaction_description = :inTransaction_description, Ammount = :inAmmount, Account_ID = :inAccount_ID; " ;
+        Map<String, Object> inputTransactionParams = new HashMap<String, Object>();
+        inputTransactionParams.put("inTransaction_type", inputTransaction.getTransactionType());
+        inputTransactionParams.put("inTransaction_time", inputTransaction.getTransactionTime());
+        inputTransactionParams.put("inState", inputTransaction.getState());
+        inputTransactionParams.put("inCategory", inputTransaction.getCategory());
+        inputTransactionParams.put("inTransaction_description", inputTransaction.getTransactionDescription());
+        inputTransactionParams.put("inAmmount", inputTransaction.getAmount());
+        inputTransactionParams.put("inAccount_ID", inputTransaction.getAccountID());
+        
+        
         try {
-            get().update(typeQuery, inputParams);
-            log.info("Transaction ID: {} successfully added", inputParams.get("inputTransaction_ID"));
+            get().update(typeQuery, inputTransactionParams);
+            log.info("Transaction Amount of {} successfully added to the Account ID: {}", inputTransaction.getAmount(), inputTransaction.getAccountID());
             return new ResponseEntity<Object>(HttpStatus.ACCEPTED);
         }  catch(NullPointerException e) {
             log.error(e.getMessage());
@@ -108,23 +147,23 @@ public class TransactionDao extends BaseDao{
     
     }
     
-    public ResponseEntity<Object> updateBuildEntry(TransactionDomain transaction) {
+    public ResponseEntity<Object> updateTransaction(TransactionDomain inputTransaction) {
 
-        String typeQuery = "update Transaction set Transaction_type = :inputTransaction_type, Transaction_time = :inputTransaction_time, State = :inputState, Category = :inputCategory, Transaction_description = :inputTransaction_description, Ammount = :inputAmmount, Account_ID = :inputAccount_ID "
-                + "WHERE TransactionID = :inputTransactionID;";
+        String typeQuery = "update Transaction set Transaction_type = :inTransaction_type, Transaction_time = :inTransaction_time, State = :inState, Category = :inCategory, Transaction_description = :inTransaction_description, Ammount = :inAmmount "
+                + "WHERE TransactionID = :inTransactionID, Account_ID = :inAccount_ID;";
         
         Map<String, Object> params = new HashMap<String,Object>();
-        params.put("inputTransaction_ID", transaction.getTransactionID());
-        params.put("inputTransaction_type", transaction.getTransactionType());
-        params.put("inputTransaction_time", transaction.getTransactionTime());
-        params.put("inputState", transaction.getState());
-        params.put("inputCategory", transaction.getCategory());
-        params.put("inputTransaction_description", transaction.getTransactionDescription());            
-        params.put("inputAmmount", transaction.getAmount());
-        params.put("inputAccount_ID", transaction.getAccountID());
+        params.put("inTransaction_ID", inputTransaction.getTransactionID());
+        params.put("inTransaction_type", inputTransaction.getTransactionType());
+        params.put("inTransaction_time", inputTransaction.getTransactionTime());
+        params.put("inState", inputTransaction.getState());
+        params.put("inCategory", inputTransaction.getCategory());
+        params.put("inTransaction_description", inputTransaction.getTransactionDescription());            
+        params.put("inAmmount", inputTransaction.getAmount());
+        params.put("inAccount_ID", inputTransaction.getAccountID());
     try {
             get().update(typeQuery, params);
-            log.info("Entry for Transaction ID: {} successfully updated", transaction.getTransactionID());
+            log.info("Transaction Amount of {} successfully updated for the Account ID: {}",  inputTransaction.getAmount(), inputTransaction.getAccountID());
             return new ResponseEntity<Object>(HttpStatus.ACCEPTED);
         } catch(NullPointerException e) {
             log.error(e.getMessage());
@@ -150,17 +189,18 @@ public class TransactionDao extends BaseDao{
      * @param int transactionId
      * @return  ResponseEntity
      */
-    public ResponseEntity<Object> deleteTransaction(int transactionID) {
+    public ResponseEntity<Object> deleteTransaction(int transactionID, int accountID) {
         
-        String delTransaction = "delete from Transaction where Transaction_ID = :inputTransaction_ID";
+        String typeQuery = "delete from Transaction where Transaction_ID = :inTransaction_ID and Account_ID = :inAccountID";
         
         Map<String, Object> params = new HashMap<String,Object>();
-        params.put("inputTransaction_ID", transactionID);
-        
+        params.put("inTransaction_ID", transactionID);
+        params.put("inAccount_ID", accountID);
+                
         
         try {
-            get().update(delTransaction, params);
-            log.info("Deleted Transaction ID: {} ", transactionID);
+            get().update(typeQuery, params);
+            log.info("Deleted Transaction ID: {} for Account ID: {}", transactionID, accountID);
             return new ResponseEntity<Object>(HttpStatus.ACCEPTED);
         } catch(NullPointerException e) {
             log.error(e.getMessage());
