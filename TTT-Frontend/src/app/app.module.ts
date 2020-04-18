9c6401f2c8629f3f0319e2a0aa248c9df38e577e
Alexa <alexas0202@gmail.com>
2020-04-17 20:53:05 -0500

    View Transactions Edit
    Added table and toolbar to view transactions

--- TTT-Frontend/src/app/app.module.ts
+++ TTT-Frontend/src/app/app.module.ts
@@ -1,25 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginScreenComponent } from './login-screen/login-screen.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
-import {MatCardModule} from '@angular/material/card';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatIconModule} from '@angular/material/icon';
-// import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MatButtonModule} from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import { ViewNotificationsComponent } from './view-notifications/view-notifications.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { EditTriggersComponent } from './edit-triggers/edit-triggers.component';
 import { ViewTransactionsComponent } from './view-transactions/view-transactions.component';
 import { AddTriggersComponent } from './add-triggers/add-triggers.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import {MatSelectModule} from '@angular/material/select';
-import {MatStepperModule} from '@angular/material/stepper';
-import {MatSliderModule} from '@angular/material/slider';
+import { MatSelectModule } from '@angular/material/select';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatTableModule} from '@angular/material/table';
 
 
 
@@ -46,10 +45,10 @@ import {MatSliderModule} from '@angular/material/slider';
     ReactiveFormsModule,
     MatSelectModule,
     MatStepperModule,
-    MatSliderModule
+    MatSliderModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-
