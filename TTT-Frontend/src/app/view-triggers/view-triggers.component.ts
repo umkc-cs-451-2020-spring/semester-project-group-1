cdb41b99d2ba0a6ed78596eb9539238370089e25
Alexa <alexas0202@gmail.com>
2020-04-19 16:44:56 -0500

    view-triggers updated
    Updated view-triggers page to placeholder table information & toolbar

--- TTT-Frontend/src/app/view-triggers/view-triggers.component.ts
+++ TTT-Frontend/src/app/view-triggers/view-triggers.component.ts
@@ -1,5 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Transactions {
+  transactionID: number;
+  transactionTime: string;
+  transactionType: string;
+  state: string;
+  category: string;
+  transactionDescription: string;
+  amount: string;
+  accountID: number;
+}
+
+const TRANSACTION_DATA: Transactions[] = [
+  {transactionID: 123, state: 'Kansas', category: 'purchase', transactionDescription: "drink", accountID: 1, transactionTime: '13:00', transactionType: 'food', amount: '12.00'},
+  {transactionID: 123, state: 'Kansas', category: 'purchase', transactionDescription: "drink", accountID: 1, transactionTime: '13:00', transactionType: 'food', amount: '12.00'},
+  {transactionID: 123, state: 'Kansas', category: 'purchase', transactionDescription: "drink", accountID: 1, transactionTime: '13:00', transactionType: 'food', amount: '12.00'},
+  {transactionID: 123, state: 'Kansas', category: 'purchase', transactionDescription: "drink", accountID: 1, transactionTime: '13:00', transactionType: 'food', amount: '12.00'},
+  {transactionID: 123, state: 'Kansas', category: 'purchase', transactionDescription: "drink", accountID: 1, transactionTime: '13:00', transactionType: 'food', amount: '12.00'},
+  {transactionID: 123, state: 'Kansas', category: 'purchase', transactionDescription: "drink", accountID: 1, transactionTime: '13:00', transactionType: 'food', amount: '12.00'},
+  {transactionID: 123, state: 'Kansas', category: 'purchase', transactionDescription: "drink", accountID: 1, transactionTime: '13:00', transactionType: 'food', amount: '12.00'},
+  {transactionID: 123, state: 'Kansas', category: 'purchase', transactionDescription: "drink", accountID: 1, transactionTime: '13:00', transactionType: 'food', amount: '12.00'},
+   
+];
+
 @Component({
   selector: 'app-view-triggers',
   templateUrl: './view-triggers.component.html',
@@ -8,6 +31,9 @@ import { Component, OnInit } from '@angular/core';
 export class ViewTriggersComponent implements OnInit {
 
   constructor() { }
+  
+  displayedColumns: string[] = ['accountID', 'transactionTime', 'transactionType', 'amount', 'transactionID', 'state', 'category', 'transactionDescription'];
+  dataSource = TRANSACTION_DATA;
 
   ngOnInit(): void {
   }
