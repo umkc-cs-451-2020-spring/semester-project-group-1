57fc75542f4c5840a1fe8effad0a0a8b842b668d
Tars <taa522@Mail.umkc.edu>
2020-04-27 14:50:59 -0500

    who needs foreign keys

--- database_setup.sql
+++ database_setup.sql
@@ -1,4 +1,4 @@
-/*drop database banking_database;*/
+drop database banking_database;
 create database banking_database;
 use banking_database;
 
@@ -13,7 +13,7 @@ create or replace TABLE account(
 	Account_ID int PRIMARY KEY,
     Account_type varchar(45),
     Balance float,
-    User_ID int,
+    User_ID int
     /*CONSTRAINT FOREIGN KEY USER_ID (User_ID) REFERENCES user(User_ID)*/
 )engine = InnoDB;
 
@@ -34,30 +34,30 @@ create or replace TABLE notification(
 	Trigger_ID int NOT null,
 	Notification_type varchar(255),
     Transaction_ID int DEFAULT NULL,
- 	Account_ID int NOT NULL,
-    CONSTRAINT FOREIGN KEY fk_Transaction	 (Transaction_ID) REFERENCES transaction(Transaction_ID),
-    CONSTRAINT FOREIGN key fk_Account (Account_ID) references account(Account_ID)
+ 	Account_ID int NOT NULL
+    /*CONSTRAINT FOREIGN KEY fk_Transaction	 (Transaction_ID) REFERENCES transaction(Transaction_ID),*/
+    /*CONSTRAINT FOREIGN key fk_Account (Account_ID) references account(Account_ID)*/
 )engine = InnoDB;
 
 create or replace TABLE state_trigger(
 	Trigger_ID int PRIMARY KEY AUTO_INCREMENT,
     User_ID int DEFAULT NULL UNIQUE,
-    Trigger_rule varchar(255),
+    Trigger_rule varchar(255)
     /*CONSTRAINT FOREIGN KEY USER_ID1 (User_ID) REFERENCES user(User_ID)*/
 )engine = InnoDB;
 
 create or replace TABLE category_trigger(
 	Trigger_ID int PRIMARY KEY AUTO_INCREMENT,
     User_ID int DEFAULT NULL UNIQUE,
-    Trigger_rule varchar(255),
-    CONSTRAINT FOREIGN KEY USER_ID2 (User_ID) REFERENCES user(User_ID)
+    Trigger_rule varchar(255)
+    /* FOREIGN KEY USER_ID2 (User_ID) REFERENCES user(User_ID)*/
 )engine = InnoDB;
 
 create or replace TABLE time_range_trigger(
 	Trigger_ID int PRIMARY KEY AUTO_INCREMENT,
     User_ID int DEFAULT NULL UNIQUE,
-    Trigger_rule varchar(255),
-    CONSTRAINT FOREIGN KEY USER_ID3 (User_ID) REFERENCES user(User_ID)
+    Trigger_rule varchar(255)
+    /* FOREIGN KEY USER_ID3 (User_ID) REFERENCES user(User_ID)*/
 )engine = InnoDB;
 
     
