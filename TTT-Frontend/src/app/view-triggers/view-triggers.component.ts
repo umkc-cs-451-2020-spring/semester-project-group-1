642903b9fb967cd07a40585b9606d908ba3166a5
daltongilmore <djgf44@mail.umkc.edu>
2020-04-26 22:17:00 -0500

    Modified TS Code for View Triggers
    implemented the example code into the component and service ts files for view triggers, changing terms to our own and deleting unneccesary lines among other needed changes for the files

--- TTT-Frontend/src/app/view-triggers/view-triggers.component.ts
+++ TTT-Frontend/src/app/view-triggers/view-triggers.component.ts
@@ -1,4 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { LoginScreenComponent } from './../login-screen/login-screen.component';
+import { ViewTransactionsComponent } from './../view-transactions/view-transactions.component';
+import { EditTriggerService } from './../edit-triggers/edit-trigger.service';
+import { TriggerTableItem } from './../transactions.service';
+import { TriggerTableService } from './../transactions.service';
+import { AfterViewInit, Component, OnInit, ViewChild, Inject } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTable, MatTableDataSource } from '@angular/material/table';
+import { TooltipPosition } from '@angular/material/tooltip';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatIconRegistry } from '@angular/material/icon';
+import {SelectionModel} from '@angular/cdk/collections';
+import {DomSanitizer} from '@angular/platform-browser';
+import { Router, ActivatedRoute} from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { trigger } from '@angular/animations';
+
+
+
 
 export interface Transactions {
   triggerNumber: number;
@@ -13,19 +34,153 @@ const TRIGGER_DATA: Transactions[] = [
   {triggerNumber: 3, triggerType: 'Timeframe', triggerSetting: '00:00-04:00',  editTrigger: 'edit'},
 ];
 
+/**
+ * The Veracode Builds Component.
+ *
+ * Displays a table of all Veracode scans pulled into the application.
+ * Empty sandbox names indicate a promoted build.
+ */
 @Component({
   selector: 'app-view-triggers',
   templateUrl: './view-triggers.component.html',
-  styleUrls: ['./view-triggers.component.scss']
+  styleUrls: ['./view-triggers.component.css']
 })
+
+
 export class ViewTriggersComponent implements OnInit {
 
-  constructor() { }
   
-  displayedColumns: string[] = ['triggerNumber', 'triggerType', 'triggerSetting', 'editTrigger'];
-  dataSource = TRIGGER_DATA;
+  /**
+   * Paginator for the table
+   */
+  @ViewChild(MatPaginator, {static: false}) paginator: MatPaginator;
 
-  ngOnInit(): void {
+  /**
+   * Sorter for the table
+   */
+  @ViewChild(MatSort, {static: false}) sort: MatSort;
+
+  /**
+   * The Application table
+   *
+   * Note that "application" in this context refers to a scanned application build on Veracode.
+   */
+  @ViewChild(MatTable, {static: false}) table: MatTable<TriggerTableItem>;
+
+  /**
+   * Build table's data source
+   */
+  dataSource = new MatTableDataSource<TriggerTableItem>();
+
+  /**
+   *  The context of the selected row
+   */
+  selection = new SelectionModel<TriggerTableItem>(true, []);
+
+  /**
+   * Tooltip position
+   */
+  abovePosition: TooltipPosition = 'below';
+
+  /**
+   * User session
+   */
+  session = window.sessionStorage;
+
+  /**
+   * Associate state passed in from the [login]{@link LoginComponent} component
+   */
+  //state$: Observable<Associate>;
+
+  /**
+   * The currently logged in user.
+   */
+  //user: Associate;
+
+  /**
+   * Please ask Tucker Diekmann what this does (DT225453)
+   */
+  displayUser = 'GUEST';
+
+  /**
+   * Please ask Tucker Diekmann what this does (DT225453)
+   */
+  loggedIn = false;
+
+  /**
+   * The array of column names for the table to use
+   */
+  displayedColumns: string[] = ['triggerNumber', 'triggerType', 'triggerSetting', 'editTrigger'];
+
+                      
+  constructor(public appTable: TriggerTableService, public dialog: MatDialog, public snackBar: MatSnackBar,
+              public iconRegistry: MatIconRegistry, public sanitizer: DomSanitizer, public router: Router,
+              public activatedRoute: ActivatedRoute) {
+    iconRegistry.addSvgIcon(
+      'view-trigger',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/icons/baseline-add_box-24px.svg'));
   }
 
-}
+  /**
+   * Refreshes the table data
+   */
+  refreshTable() {
+    this.snackBar.open('Attempting to refresh table...', 'Okay', {
+      duration: 3000
+    });
+    this.appTable.getTransaction().subscribe(data => {
+      this.dataSource.data = data;
+      this.dataSource.sort = this.sort;
+      this.dataSource.paginator = this.paginator;
+      this.snackBar.open('Table successfully loaded!', 'Okay', {
+        duration: 1000
+      });
+    }, (err) => {
+      this.snackBar.open(err, 'Okay', { duration: 10000});
+    });
+  }
+
+  /**
+   * Refreshes all listed applications by pulling all latest promoted scans
+   *//*
+  refreshApps() {
+    this.appTable.refreshTable(this.dataSource.data).subscribe(data => {
+      if (data !== '200' && data !== 200) {
+        this.snackBar.open('Error Code: ' + data + '. Error refreshing application data.', 'Close', {duration: 5000});
+      }
+    });
+  }
+*/
+  ngOnInit() {
+
+    if (this.session.getItem('user') !== 'undefined' && this.session.length > 1) {
+      /* User data to string (for session) */
+     
+      /* Login time to string (for session) */
+      const start: Date = new Date(JSON.parse(this.session.getItem('start')));
+
+      /* Calulate if 15 min have passed */
+      const now: Date = new Date();
+      const millis: number = now.getTime() - start.getTime();
+      const min = millis / 60000;
+      if (min > 30) {
+        /* Clear session and force login */
+        this.session.clear();
+        this.router.navigate(['Login']);
+      }
+      this.loggedIn = true;
+      this.refreshTable();
+    } else {
+      /* set user and login time as seesion variables */
+        const auth = this.session.getItem('authHeader');
+        this.session.clear();
+        this.session.setItem('authHeader', auth);
+      
+        this.session.setItem('start', JSON.stringify(new Date()));
+      };
+
+      this.loggedIn = true;
+      this.refreshTable();
+    }
+
+  }
--- TTT-Frontend/src/app/view-triggers/view-triggers.service.ts
+++ TTT-Frontend/src/app/view-triggers/view-triggers.service.ts
@@ -1,9 +1,79 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpHeaders, HttpParams } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type' : 'application/json'
+  })
+};
+
+
+export interface TriggerTableItem {
+  appName: string;
+  appID: number;
+  sandboxID: number;
+  buildID: number;
+  sandboxName: string;
+  businessOwner: string;
+  businessVertical: string;
+  jiraProject: string;
+  primaryLanguage: string;
+  lastStaticScan: string;
+  lastDynamicScan: string;
+  lastExcelReport: string;
+  email: string;
+  cmdb_id: number;
+  activeStatus: boolean;
+  lastModifiedBy: string;
+}
+
+@Injectable({providedIn: 'root'})
+
 export class ViewTriggersService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  // AppTableMainUrl: string = 'api/apps/all';
+  // AppTableLimit = '?_limit=15';
+
+  // TODO: Error handle the table
+
+  getApps(): Observable<TriggerTableItem[]> {
+    return this.http.get<TriggerTableItem[]>('/api/apps/all')
+    .pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  deleteApp(inputApp: TriggerTableItem): Observable<any> {
+    return this.http.delete('/api/apps/' + inputApp.appID + '/' + inputApp.buildID)
+    .pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  refreshApps(apps: TriggerTableItem[]): Observable<any> {
+    return this.http.post('/api/apps/refresh', apps)
+    .pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+
+    // clientside error
+    if (error.error instanceof ErrorEvent) {
+      console.error('An error occured:', error.error.message);
+    } else {
+      // Backend returned response code
+      console.error(`Server returned error code: ${error.status}, ` +
+      `body was: ${error.error}`);
+    }
+    return throwError(
+      'Something went wrong. Please try again or check console');
+  }
+  
 }
